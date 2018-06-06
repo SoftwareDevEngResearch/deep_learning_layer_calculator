dd1f396087fd7d3a7fa7dbdd40c90a1f69b8020a
curbsy <brianm@oregonstate.edu>
2018-06-05 23:18:08 -0700

    fixed comment

--- deep_learning_layer_calculator/tests/calculate_sizes_test.py
+++ deep_learning_layer_calculator/tests/calculate_sizes_test.py
@@ -11,10 +11,9 @@ from ..calculate_sizes import *
 
 
 #def test_import_all():
-    """ Doesn't work because Travis
-        checks if import function is importing things from csv file; not robust
-        does not check if ourput is correct because could not get file to be found in travis"""
-       
+#    """ Doesn't work because Travis
+#        checks if import function is importing things from csv file; not robust
+#        does not check if ourput is correct because could not get file to be found in travis"""
     #assert import_all('inputs.csv') != None
     
 def test_calculate_output_sizes_encoder():
